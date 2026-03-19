4089a3edaf54443a2877cbb4d423206eaf24ba65
allmybotkaze <allmybotkaze@gmail.com>
2026-03-19 13:27:54 +0800

    Update Bot_for_channel.py

--- Bot_for_channel.py
+++ Bot_for_channel.py
@@ -999,4 +999,8 @@ def main():
 
 if __name__ == "__main__":
     keep_alive()
-    main()
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    loop.run_until_complete(application.run_polling())
